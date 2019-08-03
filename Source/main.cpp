61861aacc03cbf2e2bf6a3c247ad4f5fee586e27
mind111 <806866300@qq.com>
2019-08-03 11:08:35 -0700

    -- Cleanning up and some code compression

--- Source/main.cpp
+++ Source/main.cpp
@@ -13,6 +13,11 @@ struct Vec2
     T y;
 
     //**** Constructor
+    Vec2()
+    {
+        
+    }
+
     Vec2(T p, T q) 
     {
         x = p;
@@ -53,10 +58,18 @@ struct Vec2
 
     void Swap(Vec2<T>& Another)
     {
-        Vec2<T> Holder(0, 0);
+        Vec2<T> Holder;
         Holder = *this;
         *this = Another;
         Another = Holder;
+    }
+
+    void Transpose()
+    {
+        T Holder;
+        Holder = this->x;
+        this->x = this->y;
+        this->y = Holder;
     }
 };
 
@@ -87,19 +100,17 @@ void Line(Vec2<int> Start, Vec2<int> End, TGAImage& image, const TGAColor& color
 void DrawLine(Vec2<int> Start, Vec2<int> End, TGAImage& image, const TGAColor& color)
 {
     int d = 1;
-    //**** Corner cases
-    // Divide by 0
+    //**** Divide by 0
     if (Start.x == End.x)
     {
         if (Start.y > End.y) Start.Swap(End);
         for (int i = Start.y; i <= End.y; i++)
         {
-            image.set(Start.x, i, white);
+            image.set(Start.x, i, color);
         }
 
         return;
     }
-
     // Always start from left marching toward right
     // Swap Start and End
     if (Start.x > End.x) Start.Swap(End);
@@ -107,42 +118,46 @@ void DrawLine(Vec2<int> Start, Vec2<int> End, TGAImage& image, const TGAColor& c
     float Slope = (float)(Start.y - End.y) / (float)(Start.x - End.x);
     float Intercept = (float)End.y - Slope * End.x;
     Vec2<int> Next(Start);
-    if (Slope < 0) d *= -1;
+    Vec2<int> StepA(1, 0);
+    Vec2<int> StepB(1, 1);
+
+    if (Slope < 0)
+    {
+        d *= -1;
+        StepA.Swap(StepB);
+    }
+
+    if (Slope > 1 || Slope < -1)
+    {
+        StepA.Transpose();
+        StepB.Transpose();
+    }
+
+    StepA.y *= d;
+    StepB.y *= d;
+
     while (Next.x < End.x)
     {
         //**** Slope < 1
-        if (std::abs(Slope) <= 1)
+        if (Slope >= -1 && Slope <= 1)
         {
             // Eval F(x+1,y+0.5)
             if (Next.y + d * 0.5f - Slope * (Next.x + 1.0f) - Intercept >= 0)
-                Next = Next + Vec2<int>(1, 0);
+                Next += StepA;
             else
-                Next = Next + Vec2<int>(1, d * 1);
+                Next += StepB;
         }
         /// \Bug When slope is greater than 0, can invert x, y
         else
         {
-            if (Slope < 0)
-            {
-                // Eval F(x+0.5, y+1)
-                if (Next.y + d * 1.0f - Slope * (Next.x + .5f) - Intercept >= 0)
-                    Next = Next + Vec2<int>(0, d * 1);
-                else
-                    Next = Next + Vec2<int>(1, d * 1);
-            }
-
+            // Eval F(x+0.5, y+1)
+            if (Next.y + d * 1.0f - Slope * (Next.x + .5f) - Intercept >= 0)
+                Next += StepB;
             else
-            {
-                // Eval F(x+0.5, y+1)
-                if (Next.y + d * 1.0f - Slope * (Next.x + .5f) - Intercept >= 0)
-                    Next = Next + Vec2<int>(1, d * 1);
-                else
-                    Next = Next + Vec2<int>(0, d * 1);
-            }
-
+                Next += StepA;
         }
         //**** Draw pixel to the buffer
-        image.set((int)Next.x, (int)Next.y, color);
+        image.set(Next.x, Next.y, color);
     }
 }
 
@@ -158,20 +173,27 @@ int main(int argc, char** argv) {
     // Create an image for writing pixels
     TGAImage image(200, 200, TGAImage::RGB);
 
-    /// \Note --- Line Drawing ---
-    /// \TODO Corner cases testing
-
     /// \Note --- Triangles ---
     Vec2<int> V0(10, 70);
     Vec2<int> V1(50, 160);
     Vec2<int> V2(70, 80);
 
-    DrawLine(Vec2<int>(10, 10), Vec2<int>(140, 10), image, TGAColor(30, 200, 100));
+    Vec2<int> TriangleA[3];
+    TriangleA[0] = Vec2<int>(10, 10);
+    TriangleA[1] = Vec2<int>(10, 70);
+    TriangleA[2] = Vec2<int>(60, 40);
+
+    Vec2<int> TriangleB[3];
+    TriangleB[0] = Vec2<int>(10, 20);
+    TriangleB[1] = Vec2<int>(30, 120);
+    TriangleB[2] = Vec2<int>(90, 40);
+       
+    DrawTriangle(TriangleA[0], TriangleA[1], TriangleA[2], image, TGAColor(40, 150, 100));
+    DrawTriangle(TriangleB[0], TriangleB[1], TriangleB[2], image, TGAColor(40, 150, 100));
     DrawTriangle(V0, V1, V2, image, TGAColor(200, 50, 30, 255));
-    
-    image.set(52, 41, red);
+
     image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
     image.write_tga_file("output.tga");
 
     return 0;
-}
+}   

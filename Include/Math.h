26c3a786e0a95e26aac891fe19ebd8327a4d7945
mind111 <806866300@qq.com>
2019-08-06 22:12:55 -0700

    -- Moving all the math related structs and headers to "Math.h"

---
+++ Include/Math.h
@@ -0,0 +1,134 @@
+#pragma once
+
+#include <cmath>
+#include <algorithm>
+
+template <class T>
+struct Vec2
+{
+    T x;
+    T y;
+
+    //**** Constructor
+    Vec2()
+    {
+
+    }
+
+    Vec2(T p, T q)
+    {
+        x = p;
+        y = q;
+    }
+
+    Vec2(const Vec2<T>& Another)
+    {
+        this->x = Another.x;
+        this->y = Another.y;
+    }
+
+    //**** Operations
+    Vec2<T> operator-(const Vec2<T>& Another)
+    {
+        return Vec2(this->x - Another.x, this->y - Another.y);
+    }
+
+    Vec2<T> operator+(const Vec2<T>& Another)
+    {
+        return Vec2<T>(this->x + Another.x, this->y + Another.y);
+    }
+
+    Vec2<T> operator=(const Vec2<T>& Another)
+    {
+        this->x = Another.x;
+        this->y = Another.y;
+        return *this;
+    }
+
+    Vec2<T>& operator+=(const Vec2<T>& Another)
+    {
+        this->x += Another.x;
+        this->y += Another.y;
+
+        return *this;
+    }
+
+    void Swap(Vec2<T>& Another)
+    {
+        Vec2<T> Holder;
+        Holder = *this;
+        *this = Another;
+        Another = Holder;
+    }
+
+    void Transpose()
+    {
+        T Holder;
+        Holder = this->x;
+        this->x = this->y;
+        this->y = Holder;
+    }
+};
+
+template <class T>
+struct Vec3
+{
+    T x;
+    T y;
+    T z;
+
+    //**** Constructor
+    Vec3()
+    {
+        
+    }
+
+    Vec3(T p, T q, T r)
+    {
+        x = p;
+        y = q;
+        z = r;
+    }
+
+    Vec3(const Vec3<T>& Another)
+    {
+        this->x = Another.x;
+        this->y = Another.y;
+        this->z = Another.z;
+    }
+
+    //**** Operations
+    Vec3<T> operator-(const Vec3<T>& Another)
+    {
+        return Vec3(this->x - Another.x, this->y - Another.y, this->z - Another.z);
+    }
+
+    Vec3<T> operator+(const Vec3<T>& Another)
+    {
+        return Vec3<T>(this->x + Another.x, this->y + Another.y, this->z + Another.z);
+    }
+
+    Vec3<T> operator=(const Vec3<T>& Another)
+    {
+        this->x = Another.x;
+        this->y = Another.y;
+        this->z = Another.z;
+        return *this;
+    }
+
+    Vec3<T>& operator+=(const Vec3<T>& Another)
+    {
+        this->x += Another.x;
+        this->y += Another.y;
+        this->z += Another.z;
+        return *this;
+    }
+
+    void Swap(Vec3<T>& Another)
+    {
+        Vec3<T> Holder;
+        Holder = *this;
+        *this = Another;
+        Another = Holder;
+    }
+};
---
+++ README.md
@@ -0,0 +1,18 @@
+#  Graphx
+
+##  Study Notes
+
+######  This documentation is meant as study notes that compiled from my experiences of implementing this project, so that I can keep things more organized and make it a more meaningful learning experience
+
+* This personal project is inspired by this amazing piece of material https://github.com/ssloy/tinyrenderer/wiki. It serves the purpose for me to study computer graphics out of my strong interest in the discipline.
+
+* I try to implement a software renderer from scratch following this material while not fully depending on the provided source code, since I want to struggle through the details to learn. Therefore, my implementation is probably crappy and far from optimal. If I do get stuck for a long time at some point, I would look for some hints from the material and source code.
+
+######  Line Drawing
+
+* My crappy implementation
+  * Naively interpret the math of Bresenham's line drawing to C++, which is a lot slower than the version provided in "tinyrenderer"
+  * Comparison
+    * placeholder
+    * placeholder
+
